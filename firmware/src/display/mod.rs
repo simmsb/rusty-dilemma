044bfb73e2b83fbf8a0707ac731ea55dd59c72aa
Ben Simms <ben@bensimms.moe>
2023-08-23 00:02:50 +0100

    Disable the backlight when idle

--- firmware/src/display/mod.rs
+++ firmware/src/display/mod.rs
@@ -7,6 +7,7 @@ use embassy_rp::{
     peripherals::{CORE1, PIN_11, PIN_12, PIN_13, PIN_22, PIN_23, SPI0},
     spi::Spi,
 };
+use embassy_time::Duration;
 use embedded_hal_bus::spi::ExclusiveDevice;
 use slint::platform::software_renderer::Rgb565Pixel;
 
@@ -23,7 +24,7 @@ slint::include_modules!();
 const DISPLAY_SIZE: slint::PhysicalSize = slint::PhysicalSize::new(240, 240);
 pub type TargetPixel = Rgb565Pixel;
 
-fn run(spi: SPI0, clk: PIN_22, mosi: PIN_23, cs: PIN_12, dc: PIN_11, bl: PIN_13) -> ! {
+fn run(spi: SPI0, clk: PIN_22, mosi: PIN_23, cs: PIN_12, dc: PIN_11) -> ! {
     let mut config = embassy_rp::spi::Config::default();
     config.phase = embassy_rp::spi::Phase::CaptureOnSecondTransition;
     config.polarity = embassy_rp::spi::Polarity::IdleHigh;
@@ -31,9 +32,6 @@ fn run(spi: SPI0, clk: PIN_22, mosi: PIN_23, cs: PIN_12, dc: PIN_11, bl: PIN_13)
 
     let dc = embassy_rp::gpio::Output::new(dc, embassy_rp::gpio::Level::Low);
     let cs = embassy_rp::gpio::Output::new(cs, embassy_rp::gpio::Level::Low);
-    let mut bl = embassy_rp::gpio::Output::new(bl, embassy_rp::gpio::Level::Low);
-
-    bl.set_high();
 
     let spi = Spi::new_blocking_txonly(spi, clk, mosi, config);
     let spi = ExclusiveDevice::new(spi, cs, embassy_time::Delay);
@@ -78,13 +76,32 @@ fn run(spi: SPI0, clk: PIN_22, mosi: PIN_23, cs: PIN_12, dc: PIN_11, bl: PIN_13)
 static KEYS_PRESSED: AtomicUsize = AtomicUsize::new(0);
 
 #[embassy_executor::task]
-async fn metrics_updater() {
+async fn metrics_updater(bl: PIN_13) {
     let mut sub = METRIC_UPDATES.subscriber().unwrap();
+    let mut bl = embassy_rp::gpio::Output::new(bl, embassy_rp::gpio::Level::Low);
+
+    bl.set_high();
 
     metrics::request_sync().await;
 
     loop {
-        let Metrics { keys_pressed } = sub.next_message_pure().await;
+        let Metrics { keys_pressed } = match embassy_time::with_timeout(
+            Duration::from_secs(30),
+            sub.next_message_pure(),
+        )
+        .await
+        {
+            Ok(m) => m,
+            Err(_e) => {
+                bl.set_low();
+
+                let r = sub.next_message_pure().await;
+
+                bl.set_high();
+
+                r
+            }
+        };
 
         KEYS_PRESSED.store(keys_pressed.0, atomic_polyfill::Ordering::Release);
     }
@@ -100,11 +117,11 @@ pub fn init(
     mosi: PIN_23,
     cs: PIN_12,
     dc: PIN_11,
-    rst: PIN_13,
+    bl: PIN_13,
 ) {
-    spawner.must_spawn(metrics_updater());
+    spawner.must_spawn(metrics_updater(bl));
 
     spawn_core1(core1, unsafe { &mut CORE1_STACK }, move || {
-        run(spi, clk, mosi, cs, dc, rst)
+        run(spi, clk, mosi, cs, dc)
     });
 }
